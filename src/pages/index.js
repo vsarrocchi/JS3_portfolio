ca775300df624588dc0b9c72bff32b8f863fa021
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2019-09-16 10:47:43 +0200

    add tutorial 6,7

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <div>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-    </div>
-  </Layout>
-)
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <h1>Amazing Pandas Eating Things</h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}>
+              <h3>{node.frontmatter.title}{" "}<span> — {node.frontmatter.date}</span></h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
