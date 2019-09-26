1fd014d321d33df412086eb55cd78ed4eaee865b
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2019-09-26 20:14:30 +0200

    add social media and footer

--- src/components/socialMedia.js
+++ src/components/socialMedia.js
@@ -2,13 +2,9 @@ import React from "react"
 import socialMediaStyles from "./socialMedia.module.css"
 
 export default () => (
-    // <div className={socialMediaStyles.container}>
-      // <div className={socialMediaStyles.box}>
-        <div className={socialMediaStyles.contact}>
-            <a className={socialMediaStyles.box} href="https://github.com/vsarrocchi" target="_blank"><i className="fab fa-github"></i></a>
-            <a className={socialMediaStyles.box} href="https://linkedin.com/in/valesca-sarrocchi" target="_blank"><i className="fab fa-linkedin"></i></a>
-            <a className={socialMediaStyles.box} href="mailto:valesca.sarrocchi.s@gmail.com"><i className="fa fa-envelope"></i></a>
-        </div>
-      // </div>
-    // </div>
+    <div className={socialMediaStyles.contact}>
+        <a className={socialMediaStyles.box} href="https://github.com/vsarrocchi" target="_blank"><i className="fab fa-github"></i></a>
+        <a className={socialMediaStyles.box} href="https://linkedin.com/in/valesca-sarrocchi" target="_blank"><i className="fab fa-linkedin"></i></a>
+        <a className={socialMediaStyles.box} href="mailto:valesca.sarrocchi.s@gmail.com"><i className="fa fa-envelope"></i></a>
+    </div>
 )
